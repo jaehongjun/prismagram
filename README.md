21fe8c7017df9ff8a0275d5f48b0b0a56b65bc9d
jaehongjun <gatsby0391@naver.com>
2019-04-09 22:00:09 +0900

    backend_01

---
+++ README.md
@@ -0,0 +1,18 @@
+Prismagram
+Instragram clone with Express + Prisma + React and React Native
+
+[v] User Stories
+[v] Create account
+[v]Request Secret
+Confirm Secret (Login)
+Like / Unlike a photo
+Comment on a photo
+Search by user
+Search by location
+See user profile
+Follow / Unfollow User
+See the full photo
+Edit my profile
+Upload a photo
+Edit the photo (Delete)
+See the feed
--- src/server.js
+++ src/server.js
@@ -1,15 +1,23 @@
-require("dotenv").config();
+import "./env";
 import { GraphQLServer } from "graphql-yoga";
 import logger from "morgan";
 import schema from "./schema";
+import "./passport";
+import { prisma } from "../generated/prisma-client";
+import { authenticateJwt } from "./passport";
+
+// sendSecretMail("gatsby0391@naver.com", "123");
 
 const PORT = process.env.PORT || 4000;
+// context : resolver 사이에 정보를 공유할 떄 사용
 const server = new GraphQLServer({
-  schema
+  schema,
+  context: ({ request }) => ({ request })
 });
 
 server.express.use(logger("dev"));
-
+server.express.use(authenticateJwt);
+// server.express.use(passport.authenticate("jwt"));
 server.start({ port: PORT }, () =>
   console.log(`Server running on port ${PORT}`)
 );
