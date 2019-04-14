3232940ae2d4aaec664d18633efff339f58c13ab
jaehongjun <gatsby0391@naver.com>
2019-04-14 11:21:21 +0900

    realtime and refactoring

---
+++ src/api/Room/seeRooms/seeRooms.js
@@ -0,0 +1,19 @@
+import { prisma } from "../../../../generated/prisma-client";
+import { userInfo } from "os";
+import { ROOM_FRAGMENT } from "../../../fragments";
+
+export default {
+  Query: {
+    seeRooms: (_, __, { request, isAuthenticated }) => {
+      isAuthenticated(request);
+      const { user } = request;
+      return prisma.rooms({
+        where: {
+          participants_some: {
+            id: userInfo.id
+          }
+        }
+      });
+    }
+  }
+};
